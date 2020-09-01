cb4154701b93ade9ff38c5e261540964d8b7c46d
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-09-02 03:31:12 +0900

    implement to parse request method

--- go-httptest-generator.go
+++ go-httptest-generator.go
@@ -1,7 +1,9 @@
 package generator
 
 import (
+	"fmt"
 	"go/ast"
+	"go/token"
 	"strconv"
 
 	"golang.org/x/tools/go/analysis"
@@ -17,6 +19,17 @@ var Analyzer = &analysis.Analyzer{
 	Run:  run,
 }
 
+type HandlerInfo struct {
+	URL    string
+	Method string
+}
+
+func NewHandlerInfo() *HandlerInfo {
+	handlerInfo := HandlerInfo{}
+	handlerInfo.Method = "GET"
+	return &handlerInfo
+}
+
 func run(pass *analysis.Pass) (interface{}, error) {
 	inspect := inspector.New(pass.Files)
 	nodeFilter := []ast.Node{
@@ -24,36 +37,83 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		if url, ok := httpHandleFunc(n); ok {
-			pass.Reportf(n.Pos(), "http.HandleFunc with %s", url)
+		handlerInfo := NewHandlerInfo()
+		if arg0, arg1, ok := httpHandleFunc(n); ok {
+			// Parse url.
+			basicLit, _ := arg0.(*ast.BasicLit)
+			url, err := strconv.Unquote(basicLit.Value)
+			if err != nil {
+				/* handle error */
+				return
+			}
+			handlerInfo.URL = url
+
+			// Parse handler block statement.
+			switch arg1.(type) {
+			case *ast.FuncLit:
+				funcLit, _ := arg1.(*ast.FuncLit)
+				parseHandleFuncBlock(funcLit.Body, handlerInfo)
+			case *ast.Ident:
+				// ident, _ := arg1.(*ast.Ident)
+			default:
+			}
+
+			fmt.Println(handlerInfo)
+			pass.Reportf(n.Pos(), "http.HandleFunc with %s", handlerInfo.URL)
 		}
 	})
 
 	return nil, nil
 }
 
-func httpHandleFunc(n ast.Node) (string, bool) {
+func httpHandleFunc(n ast.Node) (ast.Expr, ast.Expr, bool) {
 	callExpr, ok := n.(*ast.CallExpr)
 	if !ok {
-		return "", false
+		return nil, nil, false
 	}
 
-	selectorExpr, ok := callExpr.Fun.(*ast.SelectorExpr)
-	if !ok || selectorExpr.Sel.Name != "HandleFunc" {
-		return "", false
+	v, m, ok := accessFieldOrMethod(callExpr.Fun)
+	if !ok || v.Name != "http" || m.Name != "HandleFunc" {
+		return nil, nil, false
 	}
 
-	ident, ok := selectorExpr.X.(*ast.Ident)
-	if !ok || ident.Name != "http" {
-		return "", false
+	if len(callExpr.Args) != 2 {
+		return nil, nil, false
 	}
 
-	lit, _ := callExpr.Args[0].(*ast.BasicLit)
-	url, err := strconv.Unquote(lit.Value)
-	if err != nil {
-		// handle error
-		return "", false
+	return callExpr.Args[0], callExpr.Args[1], true
+}
+
+func parseHandleFuncBlock(block *ast.BlockStmt, handlerInfo *HandlerInfo) {
+	for _, stmt := range block.List {
+		switch stmt.(type) {
+		case *ast.IfStmt:
+			ifStmt, _ := stmt.(*ast.IfStmt)
+			binaryExpr, ok := ifStmt.Cond.(*ast.BinaryExpr)
+			if ok && binaryExpr.Op == token.NEQ {
+				v, m, ok := accessFieldOrMethod(binaryExpr.X)
+				if ok && v.Name == "r" && m.Name == "Method" {
+					basicLit, ok := binaryExpr.Y.(*ast.BasicLit)
+					if ok {
+						handlerInfo.Method, _ = strconv.Unquote(basicLit.Value)
+					}
+				}
+			}
+		default:
+		}
+	}
+}
+
+func accessFieldOrMethod(expr ast.Expr) (*ast.Ident, *ast.Ident, bool) {
+	selectorExpr, ok := expr.(*ast.SelectorExpr)
+	if !ok {
+		return nil, nil, false
 	}
 
-	return url, true
+	x, ok := selectorExpr.X.(*ast.Ident)
+	if !ok {
+		return nil, nil, false
+	}
+
+	return x, selectorExpr.Sel, true
 }
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -9,18 +9,19 @@ func test() {
 	fmt.Println("hoge")
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world")
+}
+
 func main() {
 	http.HandleFunc("/test1", func(w http.ResponseWriter, r *http.Request) { // want "http.HandleFunc with /test1"
+		if r.Method != "POST" {
+			return
+		}
 		fmt.Fprintf(w, "hello world")
 	})
 
-	http.HandleFunc("/test2", func(w http.ResponseWriter, r *http.Request) { // want "http.HandleFunc with /test2"
-		fmt.Fprintf(w, "hello world")
-	})
-
-	http.HandleFunc("/test3", func(w http.ResponseWriter, r *http.Request) { // want "http.HandleFunc with /test3"
-		fmt.Fprintf(w, "hello world")
-	})
+	http.HandleFunc("/test2", index) // want "http.HandleFunc with /test2"
 
 	test()
 }
