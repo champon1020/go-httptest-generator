2645fbaf75b067427deb400738d4daaae8d66139
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-09-01 17:51:06 +0900

    implement HandleFunc analyzer

--- go-httptest-generator.go
+++ go-httptest-generator.go
@@ -1,7 +1,11 @@
 package generator
 
 import (
+	"go/ast"
+	"strconv"
+
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 const doc = "go-httptest-generator is ..."
@@ -14,6 +18,42 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
-	// some process
+	inspect := inspector.New(pass.Files)
+	nodeFilter := []ast.Node{
+		new(ast.CallExpr),
+	}
+
+	inspect.Preorder(nodeFilter, func(n ast.Node) {
+		if url, ok := httpHandleFunc(n); ok {
+			pass.Reportf(n.Pos(), "http.HandleFunc with %s", url)
+		}
+	})
+
 	return nil, nil
+}
+
+func httpHandleFunc(n ast.Node) (string, bool) {
+	callExpr, ok := n.(*ast.CallExpr)
+	if !ok {
+		return "", false
+	}
+
+	selectorExpr, ok := callExpr.Fun.(*ast.SelectorExpr)
+	if !ok || selectorExpr.Sel.Name != "HandleFunc" {
+		return "", false
+	}
+
+	ident, ok := selectorExpr.X.(*ast.Ident)
+	if !ok || ident.Name != "http" {
+		return "", false
+	}
+
+	lit, _ := callExpr.Args[0].(*ast.BasicLit)
+	url, err := strconv.Unquote(lit.Value)
+	if err != nil {
+		// handle error
+		return "", false
+	}
+
+	return url, true
 }
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,3 +1,26 @@
 package a
 
-// some test codes
+import (
+	"fmt"
+	"net/http"
+)
+
+func test() {
+	fmt.Println("hoge")
+}
+
+func main() {
+	http.HandleFunc("/test1", func(w http.ResponseWriter, r *http.Request) { // want "http.HandleFunc with /test1"
+		fmt.Fprintf(w, "hello world")
+	})
+
+	http.HandleFunc("/test2", func(w http.ResponseWriter, r *http.Request) { // want "http.HandleFunc with /test2"
+		fmt.Fprintf(w, "hello world")
+	})
+
+	http.HandleFunc("/test3", func(w http.ResponseWriter, r *http.Request) { // want "http.HandleFunc with /test3"
+		fmt.Fprintf(w, "hello world")
+	})
+
+	test()
+}
