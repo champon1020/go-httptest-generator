bfcdc89272e589aa45eaab3280a95be5fcd005cf
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-09-04 16:28:50 +0900

    [tmp] fix testdata

--- testdata/src/handler/std/handleFunc.go
+++ testdata/src/handler/std/handleFunc.go
@@ -26,12 +26,14 @@ var indexVar2 = index
 
 func f2() {
 	var IndexVar3 = Index
+	var IndexVar4 = index
 
 	http.HandleFunc("/handleFunc1", Index)     // want "HandleFunc /handleFunc1 POST Index"
 	http.HandleFunc("/handleFunc2", index)     // Ignore
 	http.HandleFunc("/handleFunc3", IndexVar)  // want "HandleFunc /handleFunc3 POST IndexVar"
 	http.HandleFunc("/handleFunc4", IndexVar2) // want "HandleFunc /handleFunc4 POST IndexVar"
-	http.HandleFunc("/handleFunc5", IndexVar3) // Ignore
-	http.HandleFunc("/handleFunc6", indexVar)  // want "HandleFunc /handleFunc6 POST Index"
-	http.HandleFunc("/handleFunc7", indexVar2) // Ignore
+	http.HandleFunc("/handleFunc5", IndexVar3) // want "HandleFunc /handleFunc5 POST Index"
+	http.HandleFunc("/handleFunc6", IndexVar4) // Ignore
+	http.HandleFunc("/handleFunc7", indexVar)  // want "HandleFunc /handleFunc7 POST Index"
+	http.HandleFunc("/handleFunc8", indexVar2) // Ignore
 }
